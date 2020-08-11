ddb9a3691a6d27f884fcc60f14d43f0b2939e5c3
Алена <alena.lubanko@yandex.ru>
2020-08-11 14:40:46 +0300

    Rel 1

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import hbs from 'hbs';
+import path from 'path';
+
+const app = express();
+
+app.set('view engine', 'hbs');
+//process.env.PWD - путь к файлу 'public'
+app.use(express.static(path.join(process.env.PWD, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('main');
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening on ' + port));
